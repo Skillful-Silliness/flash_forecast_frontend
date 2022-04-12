9a33b308d7e77bf497c69f0ec87ab9e57a3e042d
Nick Echols <stere.ereo@gmail.com>
2022-04-11 23:19:07 -0700

    build: minimize dist files and setup github release action

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,4 +1,7 @@
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
 const { merge } = require("webpack-merge");
 
 const common = require("./webpack.common.js");
@@ -7,5 +10,12 @@ const webpackStyleRule = require("./webpackStyleRule.js");
 module.exports = merge(common, {
   mode: "production",
   module: { rules: [webpackStyleRule("production")] },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin(),
+      new CssMinimizerPlugin(),
+    ],
+  },
   plugins: [new MiniCssExtractPlugin()],
 });
